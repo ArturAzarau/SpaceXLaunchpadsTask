afc9e5cf74245ec248d3e5d1826fd9a39758d207
Artur Azarau <artur.azarau@gmail.com>
2020-04-03 01:23:09 +0300

    status showing was added to launchpad cell

--- SpaceXLaunchpads/SpaceXLaunchpads/Modules/DisplayLaunchpads/View/Cells/LaunchpadCell/LaunchpadCell.swift
+++ SpaceXLaunchpads/SpaceXLaunchpads/Modules/DisplayLaunchpads/View/Cells/LaunchpadCell/LaunchpadCell.swift
@@ -10,7 +10,7 @@ import UIKit
 
 final class LaunchpadCell: UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
         textLabel?.numberOfLines = 0
     }
     
@@ -23,6 +23,7 @@ extension LaunchpadCell: ConfigurableCell {
     
     func configure(with object: Launchpad) {
         textLabel?.text = object.fullName
+        detailTextLabel?.text = object.status
     }
 }
 
