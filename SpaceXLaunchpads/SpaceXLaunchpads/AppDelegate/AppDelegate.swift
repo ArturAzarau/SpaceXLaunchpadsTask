f4179fb7f746bf6fdde1cf28c2f2dee7e2640ecf
Artur Azarau <artur.azarau@gmail.com>
2020-04-03 02:36:58 +0300

    app delegate was refactored

--- SpaceXLaunchpads/SpaceXLaunchpads/AppDelegate/AppDelegate.swift
+++ SpaceXLaunchpads/SpaceXLaunchpads/AppDelegate/AppDelegate.swift
@@ -22,8 +22,6 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
                      moduleFactory: moduleFactory)
     }()
 
-    var window: UIWindow?
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         coordinator.start()
@@ -31,4 +29,3 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 }
-
