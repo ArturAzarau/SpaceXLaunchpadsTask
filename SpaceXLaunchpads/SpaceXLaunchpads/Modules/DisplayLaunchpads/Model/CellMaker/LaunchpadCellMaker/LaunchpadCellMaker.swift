f5de6e60342953e04c732cdfb8f90f23ee89f7dd
Artur Azarau <artur.azarau@gmail.com>
2020-04-03 03:26:30 +0300

    Cell maker was refactored

--- SpaceXLaunchpads/SpaceXLaunchpads/Modules/DisplayLaunchpads/Model/CellMaker/LaunchpadCellMaker/LaunchpadCellMaker.swift
+++ SpaceXLaunchpads/SpaceXLaunchpads/Modules/DisplayLaunchpads/Model/CellMaker/LaunchpadCellMaker/LaunchpadCellMaker.swift
@@ -6,21 +6,8 @@
 //  Copyright © 2020 arturazarau. All rights reserved.
 //
 
-import Bond
-
 final class LaunchpadCellMaker: CellMaker {
     
+    typealias ModelType = Launchpad
     typealias CellType = LaunchpadCell
-    
-    func createCell() -> TableViewBinderDataSource<OrderedCollectionChangeset<[Launchpad]>> {
-        return .init({ (launchpads, indexPath, tableView) -> UITableViewCell in
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: CellType.cellIdentifier,
-                                                           for: indexPath) as? CellType else {
-                fatalError("Invalid Cell Type")
-            }
-            
-            cell.configure(with: launchpads[indexPath.row])
-            return cell
-        })
-    }
 }
--- SpaceXLaunchpads/SpaceXLaunchpads/Modules/DisplayLaunchpads/Model/CellMaker/Protocols/CellMaker.swift
+++ SpaceXLaunchpads/SpaceXLaunchpads/Modules/DisplayLaunchpads/Model/CellMaker/Protocols/CellMaker.swift
@@ -8,10 +8,24 @@
 
 import Bond
 
-protocol CellMaker: class {
+protocol CellMaker: class where CellType.ObjectToConfigureWith == ModelType {
     
     associatedtype ModelType
     associatedtype CellType: CellWithId & ConfigurableCell
     
     func createCell() -> TableViewBinderDataSource<OrderedCollectionChangeset<[ModelType]>>
+}
+
+extension CellMaker {
+    func createCell() -> TableViewBinderDataSource<OrderedCollectionChangeset<[ModelType]>> {
+        return .init({ (models, indexPath, tableView) -> UITableViewCell in
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: CellType.cellIdentifier,
+                                                           for: indexPath) as? CellType else {
+                fatalError("Invalid Cell Type")
+            }
+            
+            cell.configure(with: models[indexPath.row])
+            return cell
+        })
+    }
 }
